f8d4d9dbb40f12777e1ff5a65ba1abd434b5700a
zhudaiwang <sjxlo@163.com>
2014-07-23 11:29:59 +0800

    修改界面为绝对布局。

--- Security/gen/com/android/tcp/R.java
+++ Security/gen/com/android/tcp/R.java
@@ -476,25 +476,25 @@ public final class R {
         public static final int write=0x7f020018;
     }
     public static final class id {
-        public static final int TvServerIP=0x7f07000a;
+        public static final int TvServerIP=0x7f070005;
         public static final int _id=0x7f070000;
         public static final int _id_ring=0x7f07000f;
-        public static final int button1=0x7f070008;
-        public static final int button2=0x7f070009;
+        public static final int button1=0x7f070009;
+        public static final int button2=0x7f07000a;
         public static final int datalist=0x7f07000d;
         public static final int datalist_ring=0x7f070012;
-        public static final int etName=0x7f070006;
-        public static final int etpass=0x7f070007;
+        public static final int etName=0x7f070007;
+        public static final int etpass=0x7f070008;
         public static final int iconring=0x7f07000e;
-        public static final int imageView1=0x7f070005;
+        public static final int imageView1=0x7f070006;
         public static final int info=0x7f07000b;
         public static final int infoBtn=0x7f07000c;
         public static final int info_ring=0x7f070011;
-        public static final int linearLayout1=0x7f070004;
         public static final int name=0x7f070002;
         public static final int name_ring=0x7f070010;
         public static final int ringtime=0x7f070001;
         public static final int state=0x7f070003;
+        public static final int tableLayout1=0x7f070004;
     }
     public static final class layout {
         public static final int data_list=0x7f030000;
